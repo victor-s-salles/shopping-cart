4267a19e0bea35a526b1a751f00cc1c6b249c160
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-09-12 19:34:06 -0300

    Adicionado valores dos produtos na pagina

--- script.js
+++ script.js
@@ -36,13 +36,14 @@ const createCustomElement = (element, className, innerText) => {
  * @param {string} product.thumbnail - URL da imagem do produto.
  * @returns {Element} Elemento de produto.
  */
-const createProductItemElement = ({ id, title, thumbnail }) => {
+const createProductItemElement = ({ id, title, thumbnail, price }) => {
   const section = document.createElement('section');
   section.className = 'item';
-
+  const priceFormated = `R$${price}`
   section.appendChild(createCustomElement('span', 'item_id', id));
   section.appendChild(createCustomElement('span', 'item__title', title));
   section.appendChild(createProductImageElement(thumbnail));
+  section.appendChild(createCustomElement('span', 'item__price', priceFormated));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
@@ -80,7 +81,7 @@ const getIdFromProductItem = (product) => product.querySelector('span.id').inner
 const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
+  li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: R$${price}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 };
